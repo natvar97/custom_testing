6749bbdaa8c48c134bb0c80228a038eba459c176
Natavar Prajapati <prajapatinatavar21197@gmail.com>
2021-07-14 17:13:43 +0530

    ordered broadcast receiver with abort functionality

---
+++ app/src/main/java/com/indialone/custom_testing/SenderBroadCastReceiver.kt
@@ -0,0 +1,28 @@
+package com.indialone.custom_testing
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.widget.Toast
+
+class SenderBroadCastReceiver : BroadcastReceiver() {
+    override fun onReceive(context: Context?, intent: Intent?) {
+        var resultCode = resultCode
+        var resultData = resultData
+        val resultExtra = getResultExtras(true)
+
+        var stringExtra = resultExtra.getString("stringExtra")
+
+        resultCode++
+        stringExtra += "->SenderBroadcast Receiver"
+
+        val toastText =
+            "SenderBroadcast Receiver\nresult code: $resultCode\nresultData: $resultData\nstring extra: $stringExtra"
+
+        Toast.makeText(context, toastText, Toast.LENGTH_SHORT).show()
+
+        resultData = "SenderBroadcast Receiver"
+        resultExtra.putString("stringExtra", stringExtra)
+        setResult(resultCode, resultData, resultExtra)
+    }
+}
